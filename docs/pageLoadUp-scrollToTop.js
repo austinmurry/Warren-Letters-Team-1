5afee1d801d33e5973cc27742baf073417a403e5
Dill <ged5131@psu.edu>
2021-12-06 12:59:36 -0500

    made edits with js

--- docs/pageLoadUp-scrollToTop.js
+++ docs/pageLoadUp-scrollToTop.js
@@ -1,5 +1,10 @@
 window.addEventListener('DOMContentLoaded',pageLoadUp,false);
 
+function pageLoadUp(){
+   var button = document.getElementById("scrollToTop")
+   button.addEventListener('click', scrollToTop, true)
+ 
+}
 function scrollToTop(){
     var scroll = window.scrollTo(0,0);
     window.scrollTo(0,0);
